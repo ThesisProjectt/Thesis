07d5e9e658f6b6be261c4b3180d1d5bb61b5f269
mootaznoomani <91527816+mootaznoomani@users.noreply.github.com>
2024-03-19 13:34:26 +0100

    login in front end

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Login from "./componant/Login"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Login/>
   )
 }
 
